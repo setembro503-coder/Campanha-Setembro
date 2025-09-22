0bbd08371f1560b34303806fccb864b464b6d335
setembro503-coder <setembro503@gmail.com>
2025-09-22 19:44:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ const STORAGE_KEY = 'parceiros_setembro_amarelo_v2';
 const parceirosPadrao = [
   { nome: 'CVV - Centro de Valorização da Vida', url: 'https://www.cvv.org.br/' },
   { nome: 'Setembro Amarelo Oficial', url: 'https://www.setembroamarelo.org.br/' },
-  { nome: 'Projeto Desabafos', url: 'https://projetodesabafos.netlify.app/' }
+  { nome: 'Projeto Desabafos', url: 'https://projetodesabafos.netlify.app/' },
   { nome: 'Oficina Bem Estar', url: 'https://patrickoliveiraqwe7-blip.github.io/Bem-estar/' }
 ];
 
@@ -146,4 +146,5 @@ function adicionarParceiro(event) {
 document.addEventListener('DOMContentLoaded', () => {
   renderizarParceiros();
 });
+
 
